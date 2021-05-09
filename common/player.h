f3f8d609a9b3c3a2867cf873ea6c7c35037edb0b
Xueyang Song <xus023@ucsd.edu>
2021-05-09 01:10:49 -0700

    change type

--- common/player.h
+++ common/player.h
@@ -7,7 +7,7 @@ struct Player {
   char max_bomb;   // how many bombs can the player place at most
   char bomb_left;  // how many bombs can the player still place
   char weapon;     // use index of object array
-  char life_left;  // number of remaining lives
+  int life_left;  // number of remaining lives
   float speed;
   char bomb_power;  // how many grids are influenced (1D)
   float gen[4][4];  // 4 * 4 float matrix used for graphics
